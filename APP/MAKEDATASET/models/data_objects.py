487bd8a7324051830d87c06c140f587388fe2bc9
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-04-11 15:11:54 -0500

    modify structure of DataToShow and DataToSave

--- APP/MAKEDATASET/models/data_objects.py
+++ APP/MAKEDATASET/models/data_objects.py
@@ -20,8 +20,10 @@ class DataFromAcquisition:
 class DataToSave:
     def __init__(self, data_acquisition: DataFromAcquisition, rgb: bool = True, depth: bool = True):
         self.data: Dict[str, np.ndarray] = {}
-        self.add_img(RGB_PREFIX, data_acquisition.rgb)
-        self.add_img(DEPTH_PREFIX, data_acquisition.depth)
+        if rgb:
+            self.add_img(RGB_PREFIX, data_acquisition.rgb)
+        if depth:
+            self.add_img(DEPTH_PREFIX, data_acquisition.depth)
         self.hour = data_acquisition.hour.strftime(HOUR_FORMAT)
 
     def add_img(self, name: str, img: np.ndarray):
@@ -30,30 +32,20 @@ class DataToSave:
 
 
 class DataToShow:
-    def __init__(self, data_acquisition: DataFromAcquisition, depth_mod: np.ndarray= None):
+    def __init__(self, data_acquisition: DataFromAcquisition, depth_mod: np.ndarray= None, zmin= ZMIN, zmax= ZMAX):
         """_summary_
 
         Args:
             data_acquisition (DataFromAcquisition): rgb-d original from stream
             depth_mod (np.ndarray, optional): a depth image that has been modify to show usefull information. it must be an 3-chanel numpy array with uint8 datatype.Defaults to None.
         """
-        self.set_range(ZMIN, ZMAX)
+        
         self.rgb = data_acquisition.rgb
         if isinstance(depth_mod, np.ndarray):
             self.depth = depth_mod
         else:
-            self.depth = depth2color(data_acquisition.depth, zmin=self.zmin, zmax= self.zmax)
+            self.depth = depth2color(data_acquisition.depth, zmin=zmin, zmax= zmax)
 
-    def set_range(self, zmin: float, zmax: float):
-        """
-        to have a better visualization of a depth image.
-        
-        zmin y zmax allow to have a better visualization of values betwin this range
-
-        Args:
-        """
-        self.zmin = zmin
-        self.zmax = zmax
 
 class DatasetTypes:
     "titles of dataset types"
