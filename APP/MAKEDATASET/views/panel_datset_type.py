f9a200108469261e2a4db7b85fa2f9398033de14
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-04-11 11:07:31 -0500

    update panel_dataset_type add documentation

--- APP/MAKEDATASET/views/panel_datset_type.py
+++ APP/MAKEDATASET/views/panel_datset_type.py
@@ -6,13 +6,15 @@ from APP.MAKEDATASET.models.data_objects import DATASET_TYPES
 
 class PanelDatasetType(CardTemplate):
     def __init__(self):
+        """ Panel where the user will select the type of datset that he want to make
+        
+        """
         super().__init__(title='Make Dataset')
         self.selected_type: str = None
         
     def init_gui(self):
         super().init_gui()
         self.combobox_datasets = QComboBox()
-        self.combobox_datasets
         self.combobox_datasets.addItems(DATASET_TYPES.as_list())
         self.set_central_widget(self.combobox_datasets)
         self.back_next_buttons.back.setEnabled(False)
@@ -21,9 +23,8 @@ class PanelDatasetType(CardTemplate):
         self.selected_type = self.combobox_datasets.currentText()
         return self.selected_type
     
-    
 #TEST
-
+################################################################################
 
 if __name__=='__main__':
     from PyQt5.QtWidgets import QApplication
