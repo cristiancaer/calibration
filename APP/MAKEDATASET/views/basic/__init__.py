cd6eb3e315c6f6932146c88afb067fcbfee6aada
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-04-11 15:07:18 -0500

    add to basic: CustomLineEdit: panel to show a title and a number value CustomSpinBox: panel to show a title and modify a number value

--- APP/MAKEDATASET/views/basic/__init__.py
+++ APP/MAKEDATASET/views/basic/__init__.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QLabel,QWidget
+from PyQt5.QtWidgets import QLabel,QWidget, QGroupBox, QHBoxLayout, QLineEdit, QSizePolicy, QSpacerItem, QSpinBox
 from PyQt5.QtCore import Qt
 
 
@@ -11,7 +11,71 @@ class LabelFont(QFont):
 
 
 class CustomLabel(QLabel):
-    def __init__(self, text: str= '', parent: QWidget= None):
+    def __init__(self, text: str= '', center: bool= False, parent: QWidget= None):
         super().__init__(parent=parent)
         self.setText(text)
-        self.setAlignment(Qt.AlignCenter)
+        self.setFont(LabelFont())
+        self.setMaximumHeight(30)
+        if center:
+            self.setAlignment(Qt.AlignCenter)
+
+
+class CustomLineEdit(QWidget):
+    def __init__(self, measure_name: str) -> None:
+        """
+        panel to show the name and the value of only one measure
+
+        Args:
+            measure_name (str): name of measure
+        """
+        super().__init__()
+        self.name = measure_name
+        self.init_gui()
+
+    def init_gui(self):
+        self.box = QGroupBox(self.name, font=LabelFont())
+        layout = QHBoxLayout(self.box)
+        self.line_edit = QLineEdit()
+        self.line_edit.setMaximumWidth(200)
+        layout.addWidget(self.line_edit)
+        horizontal_spacer = QSpacerItem(10, 10, QSizePolicy.Expanding, QSizePolicy.Fixed)
+        layout.addItem(horizontal_spacer)
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.box)
+        self.setLayout(main_layout)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+
+class CustomSpinBox(QWidget):
+    
+    def __init__(self, name: str, parent:QWidget= None)->None:
+        """
+        panel to show a title and modify a number value
+
+        Args:
+            measure_name (str): name/title of the number value
+        """
+        super().__init__()
+        self.STEP_SIZE = 500
+        self.RANGE = (0,2**16-1)# due uint16
+        self.name = name
+        self.value: int = None
+        self.init_gui()
+        
+    def init_gui(self):
+        layout = QHBoxLayout(self)
+        label_name = CustomLabel(self.name)
+        label_name.setMaximumWidth(50)
+        label_name.setMinimumWidth(0)
+        layout.addWidget(label_name)
+        self.spin_box = QSpinBox()
+        self.spin_box.setSingleStep(self.STEP_SIZE)
+        self.spin_box.setRange(*self.RANGE)
+        layout.addWidget(self.spin_box)
+    
+    def set_value(self, value: int):
+        self.spin_box.setValue(value)
+    
+    def get_value(self)-> int:
+        return self.spin_box.value()
+        
+        
