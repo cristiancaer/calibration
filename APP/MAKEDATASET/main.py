fe48b403727f6c727fa83d9e0f6e14f313396651
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-06-07 13:51:34 -0500

    update main.py: group dataset_type, choose_path, select_camera to an stack. make mill_setup and save_checking_parallel to work independently
    functional test:
    at the moment looks like all new Thread are make inside of the same core. this makes that only one core get saturated and the app crash

--- APP/MAKEDATASET/main.py
+++ APP/MAKEDATASET/main.py
@@ -1,120 +1,97 @@
-from configparser import Interpolation
-from PyQt5.QtWidgets import QMainWindow, QWidget
+from PyQt5.QtWidgets import  QWidget
 import sys
-from time import time
-import cv2
 sys.path.append('./')
-from APP.MAKEDATASET.views.integration import Integration
+from APP.MAKEDATASET.views.panel_basic_config import PanelBasicConfig
 from APP.MAKEDATASET.control.stream_source.intel_455 import Intel455
 from APP.MAKEDATASET.control.stream_source.orbbec import Orbbec
+from APP.MAKEDATASET.control.stream_source.test import ImageGenerator
 from APP.MAKEDATASET.control.stream_source.thread_stream import ThreadToStream
-from APP.MAKEDATASET.control.save_pair_images import ThreadToSave
-from APP.MAKEDATASET.models.data_objects import DATASET_TYPES, DataToSave, DataFromAcquisition, DataToShow, DatasetTypes
-from APP.MAKEDATASET.views.draw_tools.draw_over_data_to_show import DrawDataToShow
-from APP.MAKEDATASET.control.check_parallel.through_square import ThroughSquare
+from APP.MAKEDATASET.models.data_objects import DATASET_TYPES, DatasetTypes
+from APP.MAKEDATASET.views.stack import StackWidget
+from APP.MAKEDATASET.control.app_panels.setup_mill import MillSetup
+from APP.MAKEDATASET.control.app_panels.save_checking_parallel import SaveCheckingParallel
+        
 
-
-class MainWindow(QMainWindow):
+class MainWindow(StackWidget):
     def __init__(self, window_name = '', stream_handler: ThreadToStream= None, parent: QWidget=None):
         self.stream_handler = stream_handler
-        super().__init__(parent)
         self.window_name = window_name
+        super().__init__(parent)
         self.init_gui()
         self.setup()
         
     def init_gui(self):
         self.setWindowTitle(self.window_name)
-        self.panels = Integration()
-        self.setCentralWidget(self.panels)
-        self.panels.panel_to_save.buttom_new_dataset.clicked.connect(self.new_dataset)
-        self.panels.panel_to_save.panel_rgb_d.set_dark_images()
+        self.panel_stack_basic_config = PanelBasicConfig()
+        self.add_panel(self.panel_stack_basic_config)
+        self.pane_setup_mill = MillSetup(self.stream_handler)
+        self.pane_setup_mill.disconnect_stream()# by default if the stream data is ready start to show in panel. this avoid update the image visualization
+        self.add_panel(self.pane_setup_mill)
+        self.panel_save = SaveCheckingParallel(self.stream_handler, top_point=[100,100], bottom_point=[-100,-100])
+        self.add_panel(self.panel_save)
+        print(f'id panel save :{id(self.panel_save)}')
         # self.showMaximized()
         
     def setup(self):
         # stream config
-        
         self.stream_handler.add_stream(Intel455())
         self.stream_handler.add_stream(Orbbec())
+        self.stream_handler.add_stream(ImageGenerator())
         self.stream_handler.start()
-        self.stream_handler.data_ready.connect(self.process_images)
-        self.stream_handler.data_ready.connect(self.save_images)
-        self.panels.panel_select_camera.button_update_streams.clicked.connect(self.update_available_streams)
-        # self.panels.panel_select_camera.combobox_cameras.currentTextChanged.connect(self.set_stream)
-        self.panels.panel_to_save.panel_rgb_d.button_reconnect_stream.clicked.connect(self.set_stream)
-        
-        # save config
-        self.save_handler = ThreadToSave()
-        self.save_handler.start()
-        self.is_saving = False
-        self.panels.panel_choose_path.back_next_buttons.next.clicked.connect(self.update_save_info)
-        self.panels.panel_to_save.button_save.clicked.connect(self.change_status_saving)
-        self.save_handler.last_index.connect(self.panels.panel_to_save.line_edit_last_saved.update_text)
-        
-        # check config
-        self.check_parallel = ThroughSquare(top_point=[100,170], bottom_point=[-100,-100], threshold=10, line_width= 15)
-        
-    def update_save_info(self):
-        path = self.panels.panel_choose_path.path
-        self.save_handler.set_path(path)
-        self.panels.panel_to_save.line_edit_last_saved.update_title(path)
-        if self.panels.dataset_type == DATASET_TYPES.MILL:
-            self.save_handler.set_index_type(datatime_index=True)
+        # update available streams
+        # self.update_available_streams()
+        self.panel_stack_basic_config.panel_select_camera.button_update_streams.clicked.connect(self.update_available_streams)
+        # go at click in last next button in basic config
+        self.panel_stack_basic_config.set_function_to_going(self.f_button_next_from_basic_config)
+        # go at click in next button mill setup
+        self.pane_setup_mill.set_function_to_going(self.at_going_to_pane_save)
+        # go at click in new dataset button
+        self.panel_save.set_function_to_going(self.f_button_new_dataset)
+        # connect back buttons
+        super().setup()
         
     def update_available_streams(self):
-        self.panels.panel_select_camera.update_available_streams(self.stream_handler.get_available_stream_names())
-
+        self.panel_stack_basic_config.panel_select_camera.update_available_streams(self.stream_handler.get_available_stream_names())
+        
     def set_stream(self):
-        stream_name = self.panels.panel_select_camera.get_selected()
+        stream_name = self.panel_stack_basic_config.panel_select_camera.get_selected()
         self.stream_handler.set_stream(stream_name)
-        if hasattr(self.stream_handler.actual_stream,'depth_units'):
-            self.panels.panel_to_save.panel_rgb_d.panel_visualization_range.set_units(self.stream_handler.actual_stream.depth_units)
+        
     
-    def process_images(self, data:DataFromAcquisition)->None:
-        dataset_type = self.panels.panel_dataset_type.get_selected()
-        zmin, zmax = self.panels.panel_to_save.panel_rgb_d.panel_visualization_range.get_range() 
-        data_to_show = DrawDataToShow(data, zmin, zmax)
-        if dataset_type == DATASET_TYPES.AREA_CALIBRATION or dataset_type == DATASET_TYPES.Z_CALIBRATION:
-            # if self.panels.panel_select_camera.get_selected() == Intel455.name:
-            #     percent = 0.6
-            #     t = time()
-            #     data_to_show.WIDTH = int(data_to_show.WIDTH * percent)
-            #     data_to_show.HEIGHT = int(data_to_show.HEIGHT* percent)
-            #     dim = (data_to_show.WIDTH, data_to_show.HEIGHT)
-            #     interpol =  cv2.INTER_CUBIC
-            #     data_to_show.rgb = cv2.resize(data_to_show.rgb, dim, interpolation= interpol)
-            #     data_to_show.depth = cv2.resize(data_to_show.depth, dim, interpolation= interpol)
-            #     print(time()-t)
-            # print(data_to_show.rgb.shape)    
-            y_status, x_status = self.check_parallel.check(data.depth)
-            data_to_show.draw_parallel_information(y_status, x_status)
-            data_to_show.draw_rectangle(self.check_parallel.top_point[::-1], self.check_parallel.bottom_point[::-1])# original are in array index reference [row, column]. draw has [x,y] index reference
-        self.panels.panel_to_save.panel_rgb_d.update_rgbd(data_to_show)
-    
-    def save_images(self, data:DataFromAcquisition)->None:
-        data_to_save = DataToSave(data)
-        if self.is_saving:
-            self.save_handler.add_new(data_to_save)
-        dataset_type = self.panels.panel_dataset_type.get_selected()
-        if dataset_type != DATASET_TYPES.MILL:
-            self.is_saving = False
-    
-    def change_status_saving(self):
-        self.is_saving = not self.is_saving
-    
-    def new_dataset(self):
-        self.stream_handler.reset_stream()
-        self.panels.panel_to_save.panel_rgb_d.set_dark_images()
-        self.panels.go_to_panel(self.panels.panel_dataset_type)
-        # clear displayed path
-        self.panels.panel_choose_path.back_next_buttons.next.setEnabled(False)
-        self.panels.panel_choose_path.button_path.set_text('')
+    def f_button_next_from_basic_config(self):
+        for panel in self.list_panels:
+            print(panel.name, id(panel))
+        self.set_stream()
+        if self.panel_stack_basic_config.panel_dataset_type.get_selected()==DatasetTypes.MILL:
+            self.at_going_to_setup_mill()
+        else:
+            self.at_going_to_pane_save()  
+        
+    def f_button_new_dataset(self):
+        self.stream_handler.disconnect_data_ready_slots()
+        self.panel_stack_basic_config.go_to_panel(self.panel_stack_basic_config.panel_dataset_type)
+        self.go_to_panel(self.panel_stack_basic_config)
+        
+        
+    def at_going_to_setup_mill(self):
+        self.go_to_panel(self.pane_setup_mill)
+        self.stream_handler.disconnect_data_ready_slots()# this will disconnect all slots connected to data_ready signal
+        self.pane_setup_mill.connect_stream()
+        
+    def at_going_to_pane_save(self):
+        self.go_to_panel(self.panel_save)
+        self.stream_handler.disconnect_data_ready_slots()# this will disconnect all slots connected to data_ready signal
+        self.panel_save.connect_stream()
+        dataset_type = self.panel_stack_basic_config.panel_dataset_type.get_selected()
+        path = self.panel_stack_basic_config.panel_choose_path.path
+        self.panel_save.update_save_info(dataset_type, path)
+        self.panel_save.update_stream_info()
+        
         
     def closeEvent(self, event) -> None:
         self.stream_handler.close()
-        self.save_handler.close()
         return super().closeEvent(event)
     
-
 #TEST
 ################################################################################
 if __name__=='__main__':
