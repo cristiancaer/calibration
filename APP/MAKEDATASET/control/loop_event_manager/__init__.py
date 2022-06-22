f94743a414a238865b77cb614df0d720818a91ef
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-06-22 15:07:36 -0500

    change loop_event_manager: turn LoopEventManager into a base-LoopEventManager where the poolTread will be configurate in another clase

--- APP/MAKEDATASET/control/loop_event_manager/__init__.py
+++ APP/MAKEDATASET/control/loop_event_manager/__init__.py
@@ -1,19 +1,15 @@
 from typing import Dict, List
-from time import sleep
-import numpy as np
-from threading import Thread
 from queue import Queue
-from concurrent.futures import ThreadPoolExecutor
 import traceback
 from types import FunctionType
 import sys
 sys.path.append('./')
-from APP.MAKEDATASET.models.data_objects import DataFromAcquisition, EventData
+from APP.MAKEDATASET.models.data_objects import  EventData
 from APP.MAKEDATASET.control.loop_event_manager.signal_handler import SignalHandler
 
 
-class LoopEventManager(Thread):
-    def __init__(self, name: str, max_workers=15) -> None:
+class LoopEventManager():
+    def __init__(self, max_workers=15) -> None:
         """
         A Thread-class that is used to initialize a thread by each function stored in the signal_handlers.
         there may be several signal_handler
@@ -21,12 +17,9 @@ class LoopEventManager(Thread):
         args:
         name[str]: prefix_name for threads initialized in threadPoolExecutor
         """
-        super().__init__()
         max_workers = max_workers
-        thread_name_prefix = name
         self.running = True  # to stop the while in running loop function
-        self.event_executor = ThreadPoolExecutor(
-            max_workers, thread_name_prefix)
+
         # a buffer to put  a name and the args to work with a signal_handler
         self.buffer_event = Queue(50)
         # to store several signal_handlers and  its names
@@ -92,14 +85,8 @@ class LoopEventManager(Thread):
                         event_data.name)
                     list_to_do = work_handler.get_list_to_do()
                     for to_do_function in list_to_do:
-                        if work_handler.Data_type_to_work != type(None):
-                            if self.running:
-                                self.event_executor.submit(
-                                    to_do_function, event_data.data_to_work)
-                        else:
-                            # this is need to run functions with not args
-                            print('run function without args')
-                            self.event_executor.submit(to_do_function)
+                        self.add_new_thread_task(to_do_function,event_data.data_to_work)
+                       
             except Exception:
                 print(f'error in emit function from {event_data.name}')
                 print(traceback.format_exc())
@@ -107,6 +94,9 @@ class LoopEventManager(Thread):
                 break
         print('close loop-thread')
 
+    def add_new_thread_task(self, todo_function:FunctionType, data_to_work:any) -> None:
+        pass
+    
     def check_running(self):
         return self.running
 
@@ -119,60 +109,3 @@ class LoopEventManager(Thread):
         self.running = False
         self.buffer_event.put(None)
 
-# TEST
-# ##########################################
-
-
-def run():
-    def make_task(name: str):
-
-        # todo
-        def do_something(data_to_work: DataFromAcquisition):
-            # the function must be build with the same datatype-args with which the data handler was instanced
-            print(
-                f'work {name}: shape rgb: {data_to_work.rgb.size},depth: {data_to_work.depth.size}')
-            print('')
-
-        return do_something
-
-    loop_manager = LoopEventManager('pool manager')
-    loop_manager.start()
-    loop_manager.emit_function_only_once(lambda: print(
-        'run only once and not repeat'), wrap_in_loop=False)
-    work1_handler = SignalHandler(
-        name='process1', data_type_to_work=DataFromAcquisition)
-    work2_handler = SignalHandler(
-        name='process2', data_type_to_work=DataFromAcquisition)
-    loop_manager.add_signal_handlers_from_list([work1_handler, work2_handler])
-    work1_handler.connect(make_task(work1_handler.name))
-    work2_handler.connect(make_task(work2_handler.name))
-    # work2_handler.emit(9)
-
-    def loop_emit_triggers():
-        print('trigger loop')
-        loop_manager.emit_function_only_once(lambda: print(
-            'run only once and not repeat'))  # run function without handler
-        # do something...get something...
-        img = np.zeros((640, 480))
-        # the work_handler  is  going to work  only with the data type with which they were instantiated and with no args( args=None, behavior by default)
-        # this is expected and valid
-        work1_handler.emit(DataFromAcquisition(img, img))
-        sleep(2)
-        # the emit always must have the same datatype with which the data handler was instantiated
-        work2_handler.emit(DataFromAcquisition(2*img, 2*img))
-        sleep(3)
-        # work2_handler.emit(9)# 9 is a int-var-type then it's not valid, this is not the data-type(DataFromAcquisition) with which the handler was instantiated
-    # make test signals-emit
-    loop_manager.emit_function_only_once(
-        loop_emit_triggers, wrap_in_loop=True)  # run function without handler
-
-    # work1_handler.emit()
-    while True:
-        check_input = input('press q to close')
-        if check_input == 'q':
-            break
-    loop_manager.stop()
-
-
-if __name__ == '__main__':
-    run()
