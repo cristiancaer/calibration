56c9e9e94fb885a03b4cb01c5dab70e51c59c3b9
Cristian Espinosa <cristianespinosacaer@gmail.com>
2022-09-12 10:47:27 -0500

    add basic fields to the config file

--- APP/models/basic_config.py
+++ APP/models/basic_config.py
@@ -1,11 +1,15 @@
 TEST_IMG_PATH = './images/'
+TEST_IMG_Z_PATH = './images/z/'
+TEST_IMG_AREA_PATH = './images/area/'
 RGB_PREFIX = 'rgb'
 DEPTH_PREFIX = 'depth'
 HOUR_FORMAT = "%Y_%m_%d_%H_%M_%S_%f"
 ZMIN = 4000
-ZMAX = 20000
+ZMAX = 25000
 IMAGE_SHAPE = (480, 640)
 ################################################################################
 CC_ICON_PATH='APP/views/basic/icons/cc.jpg'
 TEST_IMG=CC_ICON_PATH
 TEST_PATH = './test'
+CONFIG_PATH = TEST_PATH
+CONFIG_NAME= 'config'
